6c1c289c7839b5d77b464db8dc0560f8d968e3f1
yanhui <865809599@qq.com>
2020-09-24 23:20:20 +0800

    合并二叉树

---
+++ Week_03/617.合并二叉树.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode.cn id=617 lang=python3
+#
+# [617] 合并二叉树
+#
+# https://leetcode-cn.com/problems/merge-two-binary-trees/description/
+#
+# algorithms
+# Easy (77.25%)
+# Likes:    508
+# Dislikes: 0
+# Total Accepted:    87.8K
+# Total Submissions: 112.4K
+# Testcase Example:  '[1,3,2,5]\n[2,1,3,null,4,null,7]'
+#
+# 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+# 
+# 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL
+# 的节点将直接作为新二叉树的节点。
+# 
+# 示例 1:
+# 
+# 
+# 输入: 
+# Tree 1                     Tree 2                  
+# ⁠         1                         2                             
+# ⁠        / \                       / \                            
+# ⁠       3   2                     1   3                        
+# ⁠      /                           \   \                      
+# ⁠     5                             4   7                  
+# 输出: 
+# 合并后的树:
+# 3
+# / \
+# 4   5
+# / \   \ 
+# 5   4   7
+# 
+# 
+# 注意: 合并必须从两个树的根节点开始。
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+'''
+思路：
+1、题目中提示需要从两个树的根节点开始，就需要按着根-左-右的顺序处理，感觉写法和前序遍历差不多
+2、需要注意校验树的子节点是否存在
+ 
+'''
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+        mergeTree = TreeNode(t1.val + t2.val)
+        mergeTree.left = self.mergeTrees(t1.left, t2.left)
+        mergeTree.right = self.mergeTrees(t1.right, t2.right)
+        return mergeTree
+# @lc code=end
